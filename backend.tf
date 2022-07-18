65bb4d313b90ed45d65e1c754e763243470117de
silpapriyadarshini <silpa.priyadarshini@cloudreach.com>
2022-07-18 19:06:47 +0530

    adding dynamodb for lock table

--- backend.tf
+++ backend.tf
@@ -2,5 +2,7 @@ terraform {
     backend "s3" {
         bucket = "talent-academy-silpa-lab-tfstates"
         key = "talent-academy/backend/terraform.tfstates"
+        region = "eu-west-1"
+        dynamodb_table = "terraform-lock"
     }
 }
